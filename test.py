44ca9dd382b516e909cfe7ec4c79322c32a1c7cc
primw <prim@nevada.unr.edu>
2021-04-26 23:21:23 -0700

    unit tests

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
-from semaphoreflask import app
+from semaphoreflask import app, mongo
 import unittest
 
 class TaskTest(unittest.TestCase):
 
+    #Tests for a connection to the home page url
     def test_homepage(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+    
+    #Tests for a connection to the all posts url
+    def test_all_posts_page(self):
+        tester = app.test_client(self)
         response = tester.get('/task/all', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    #Test for all page connections + mock input and assertEqual using tasks = mongo.db.tasks.find() i.e routes.py
+    def test_create_post(self):
+        task_title = "test"
+        task_description = "test-post"
+        task_created_at = "NULL"
+        mongo.db.tasks.insert_one({'task_title': task_title, 'task_description': task_description, 'task_created_at': task_created_at})
+        tasks_data = []
+        if mongo:
+            tasks = mongo.db.tasks.find()
+            if tasks:
+                for task in tasks:
+                    print(task)
+                    tasks_data.append({'task_id': task['_id'], 'task_title': task['task_title'], 'task_description': task['task_description'],
+                    'task_created_at': task['task_created_at']}) 
         
+        self.assertEqual(tasks_data[0].task_title, "test")
+
 if __name__ == '__main__':
     unittest.main()
