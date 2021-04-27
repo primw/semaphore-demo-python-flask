d712e705baa19b7c0d1727b6aae8d688c4eab189
primw <prim@nevada.unr.edu>
2021-04-26 17:55:13 -0700

    added front end changes

--- semaphoreflask/forms.py
+++ semaphoreflask/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField, DateTimeField, SubmitField
 from wtforms.validators import DataRequired
 
 class CreateTaskForm(FlaskForm):
-    task_title = StringField('Task Title', validators=[DataRequired()])
-    task_description = TextAreaField('Task Description')
-    task_submit = SubmitField('Create Task')
+    task_title = StringField('Username', validators=[DataRequired()])
+    task_description = TextAreaField('Post Contents')
+    task_submit = SubmitField('Create Post')
